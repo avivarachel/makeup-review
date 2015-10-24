2b6be9f19a4c3abdc2892ea15d9d541f0a616af4
Ulaize <ulaize@creative-tweed.com>
2015-10-24 00:22:06 +0000

    images added to the product list

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ $('#submit').on("click", function(evnt) {
                     $app.append($div);
                     var name = product.product_name;
                     var brand = product.brand_name;
+                    var img = ('https://d3gm19tlfubzts.cloudfront.net/images_products/' + product.hash_url_image +'.jpg');
+                    $div.append('<image src=' + img + '>');
                     $div.append('<p class=name>' + name + '</p>');
                     $div.append('<p class=brand>' + brand + '</p>');
 
@@ -31,13 +33,16 @@ $('#submit').on("click", function(evnt) {
                             
                             var name = $(this).find('p.name').html();
                             var brand = $(this).find('p.brand').html();
-                            var youtubeCall = ('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + name + brand + ' review&key=AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE');
-                            $.getJSON(youtubeCall).items.forEach(function(video){
-                                console.log(video[0]);
-        
-                            }
+                        
                             
-                            );
+                            var youtubeCall = ('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + name + brand + ' review&key=AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE');
+                            $.getJSON(youtubeCall).then(function(response){
+                                response.items.forEach(function(video){
+                                    var iframe = ('<iframe width="560" height="315" src="https://www.youtube.com/embed/' + video.id.videoId + '">');
+                                    $app.find('div').after(iframe);
+                                });
+                            });
+                            
                             //     var $body = $('#body');
                             //     $body.html('');
                             //     var $head = $('#head');
