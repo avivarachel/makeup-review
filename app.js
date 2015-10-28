62e2d05c72e9fcf61271eb04cf500d2ea525292a
Aviva Wolpert <wolpertaviva@gmail.com>
2015-10-28 22:14:21 +0000

    search bar 2.0

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var $app = $('#app');
 
 
-var $header = $('<div id="header"><a href="#"><img src="assets/home.png" ></a><form><input type="search" class="search-input" id="header-search-bar" placeholder="search for your favourite makeup"><button class="submit"></button></div>');
+var $header = $('<div id="header"><a href="#"><img src="assets/home.png" ></a><form><input type="search" class="search-input" id="header-search-bar" placeholder="search..."><button class="submit"></button><img src="assets/magnifier.png"></form></div>');
 
 
 
