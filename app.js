2c6e5a593b61045cd4a847907133545298049550
Aviva Wolpert <wolpertaviva@gmail.com>
2015-10-28 01:19:29 +0000

    video grid

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-//Reusable vars
+//Reusable elements
 
 var $app = $('#app');
+var $header = $('<div id="header"><form><input type="search" class="search-input" id="header-search-bar" placeholder="search for your favourite makeup <i class=fa fa-search"></i> "><button class="submit"></button></div>');
 
-
-var $header = $('<div id="header"></div><form><input type="search" id="searchbar" placeholder="search for your favourite makeup">');
 var API_URL = "http://www.murnow.com/api/search/?q=";
 
-
+function search() {
+    $('.submit').on("click", function(evnt) {
+        evnt.preventDefault();
+        var input = $('.search-input').val();
+        appRouter.navigate('search/?q=' + input, {
+            trigger: true
+        });
+    });
+}
 
 
 //Backbone router
@@ -14,13 +21,12 @@ var API_URL = "http://www.murnow.com/api/search/?q=";
 var AppRouter = Backbone.Router.extend({
     routes: {
         '': 'home',
-        'search/?q=:query': 'search',
-
-        'product/:productId': 'product'
+        'search/?q=:query': 'productList',
+        'product/:productId': 'productView'
     },
     home: productSearch,
-    search: productList,
-    product: productView
+    productList: productList,
+    productView: productView
 });
 
 var appRouter = new AppRouter();
@@ -29,111 +35,112 @@ Backbone.history.start();
 // Home page search view
 function productSearch() {
     $app.html('');
-    var $search = $('<div id="search">');
+    var $homeDiv = $('<div id="home-div">');
 
-    $('#search > form > input[type="search"]').focus();
-    $app.append($search);
-    $search.append("<form><input type='search' id='search-input' placeholder='search for your favourite makeup'><button id='submit'>Search</button></form>");
-    
-    $('#submit').on("click", function(evnt) {
-        // console.log('#submit was clicked');
-        evnt.preventDefault();
-        var input = $('#search-input').val();
-        appRouter.navigate('search/?q=' + input, {trigger: true});
+    $('#home-div > form > input[type="search"]').focus();
+    $app.append($homeDiv);
+    $homeDiv.append("<form><input type='search' class='search-input' id='home-search-bar' placeholder='search for your favourite makeup'><button class='submit'>Search</button></form>");
+    search();
 
-    });
 }
 
 //Product list view
+function productList(input, pageNum) {
+    $.getJSON(API_URL + input).then(function(response) {
 
-function productList(search,pageNum){
+        if (response.search.length === 0) {
+            $app.html(''); // Clear the #app div
+            $app.append($header);
+            search();
+            $app.append('<h1>We do not have what you are looking for. Search again</h1>');
+        }
 
-    $.getJSON(API_URL + search).then(function(response) {
-        $app.html(''); // Clear the #app div
-        $app.append($header);
-        //ul needs to be appended to the div
+        else {
+            $app.html(''); // Clear the #app div
+            $app.append($header);
+            search();
+            //ul needs to be appended to the div
 
-    
-        var $ul = $('<ul class="large-block-grid-5">');
-        $app.append($ul);
-        
+            var $ul = $('<ul class="large-block-grid-5">');
+            $app.append($ul);
 
-        response.search.forEach(function(product) {
-            var $li = $('<li>');
-            $ul.append($li);
-            var name = product.product_name;
-            var brand = product.brand_name;
+            response.search.forEach(function(product) {
+                var $li = $('<li>');
+                $ul.append($li);
+                var name=product.product_name.substring(0,30);
+                if (name.length>29){
+                    name =  name +'...';
+                }
+                var brand = product.brand_name;
+                var img = ('https://d3gm19tlfubzts.cloudfront.net/images_products/' + product.hash_url_image + '.jpg');
 
-            var img = ('https://d3gm19tlfubzts.cloudfront.net/images_products/' + product.hash_url_image +'.jpg');
-    
-            var $a = $('<a href="#product/' + product.id + '">');
-            $li.append($a);
-                
+                var $a = $('<a href="#product/' + product.id + '">');
+                $li.append($a);
 
-            $a.append('<image src=' + img + '>');
-            $a.append('<p class=name>' + name + '</p>');
-            $a.append('<p class=brand>' + brand + '</p>');
-        });
-
-        
-        //Button and load more function
-        var $button = $('<button id="more-products">Load more!</button>');
-        $app.append($button);
-        $('#more-products').on("click", function(evnt) {
-            pageNum++;
-            $.getJSON(API_URL + search + '&from=' + (pageNum*20)).then(function(response) {
-                response.search.forEach(function(product) {
-                    var $li = $('<li>');
-                    $app.find('ul').append($li);
-                    var name = product.product_name;
-                    var brand = product.brand_name;
-                    var img = ('https://d3gm19tlfubzts.cloudfront.net/images_products/' + product.hash_url_image +'.jpg');
-            
-                    var $a = $('<a href="#product/' + product.id + '">');
-                    $li.append($a);
-                
-                    $a.append('<image src=' + img + '>');
-                    $a.append('<p class=name>' + name + '</p>');
-                    $a.append('<p class=brand>' + brand + '</p>');
-                });
-                
+                $a.append('<image src=' + img + '>');
+                $a.append('<p class=name>' + name + '</p>');
+                $a.append('<p class=brand>' + brand + '</p>');
             });
-        });
-            
+
+
+            //Button and load more function
+            var $loadMoreProducts = $('<button id="more-products">Load more!</button>');
+            $app.append($loadMoreProducts);
+            $('#more-products').on("click", function(evnt) {
+                pageNum++;
+                $.getJSON(API_URL + input + '&from=' + (pageNum * 20)).then(function(response) {
+                    response.search.forEach(function(product) {
+                        var $li = $('<li>');
+                        $app.find('ul').append($li);
+                        var name = product.product_name;
+                        var brand = product.brand_name;
+                        var img = ('https://d3gm19tlfubzts.cloudfront.net/images_products/' + product.hash_url_image + '.jpg');
+
+                        var $a = $('<a href="#product/' + product.id + '">');
+                        $li.append($a);
+
+                        $a.append('<image src=' + img + '>');
+                        $a.append('<p class=name>' + name + '</p>');
+                        $a.append('<p class=brand>' + brand + '</p>');
+                    });
+
+                });
+            });
+        }
     });
 }
 
 
 //Product view with videos
 
-function productView(productId){
+function productView(productId) {
     $.getJSON('http://www.murnow.com/api/products/' + productId).then(
         function(product) {
-            
             var name = product.product.product_name;
             var brand = product.product.brand_name;
-            var img = ('https://d3gm19tlfubzts.cloudfront.net/images_products/' + product.product.hash_url_image +'.jpg');
+            var img = ('https://d3gm19tlfubzts.cloudfront.net/images_products/' + product.product.hash_url_image + '.jpg');
+
+
             $app.html('');
+            $app.append($header);
+            search();
+
+
             var $productProfile = $('<div id="productProfile" class="row">');
             $app.append($productProfile);
             $app.find('#productProfile').append('<div class="large-5 columns"><image src=' + img + '>');
             $app.find('#productProfile').append('<div class="large-7 columns"><h1>' + name);
             $app.find('#productProfile').append('<div class="large-7 columns"><h2>' + brand);
-            
+
             var youtubeCall = ('https://www.googleapis.com/youtube/v3/search?part=snippet&q=' + name + brand + ' review&key=AIzaSyCR5In4DZaTP6IEZQ0r1JceuvluJRzQNLE');
-            $.getJSON(youtubeCall).then(function(response){
-               
-                response.items.forEach(function(video){
-                   
-                    $app.append('<div id="video"><iframe width="560" height="315" src="https://www.youtube.com/embed/' + video.id.videoId + '">');
+            $.getJSON(youtubeCall).then(function(response) {
+                
+                $app.append('<div id=videos class=row>');
+                response.items.forEach(function(video) {
+
+                    $app.find('#videos').append('<div class="small-12 medium-6 large-4 columns"><iframe src="https://www.youtube.com/embed/' + video.id.videoId + '">');
                 });
             });
         }
-    )
+    );
 }
-           
-                            
-                                
-                            
-                        
-
